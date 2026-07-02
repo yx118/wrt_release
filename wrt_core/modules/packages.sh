1b41bfd342ae36dd514189c224b59fabcb81db5f
yx118 <yx@yxoyx.xyz>
2026-07-02 09:54:48 +0800

    Update packages.sh
    Signed-off-by: yx118 <yx@yxoyx.xyz>

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -210,7 +210,7 @@ install_custom_feed() {
         luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest netdata luci-app-netdata \
         lucky luci-app-lucky luci-app-openclash luci-app-homeproxy luci-app-amlogic \
         oaf open-app-filter luci-app-oaf easytier luci-app-easytier \
-        msd_lite luci-app-msd_lite cups luci-app-cupsd
+        msd_lite luci-app-msd_lite cups luci-app-cupsd luci-app-pushbot
     )
     local required_feed_dirs=(
         cups tcping v2ray-geodata luci-lib-taskd luci-app-openclash
